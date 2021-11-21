5e9494128b6976ba2ae2726a8f1214f7cc373945
J. Aveiro <jpd.aveiro@gmail.com>
2021-11-21 19:54:55 +0000

    Improve styling

--- AO_modules/source.py
+++ AO_modules/source.py
@@ -2,10 +2,10 @@ import inspect
 
 import numpy as np
 
-from photometry import Phot
+from photometry import Photometry
 
 
-# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% CLASS INITIALIZATION %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% CLASS INITIALIZATION %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 class Source:    
     def __init__(self, opt_band, magnitude):
@@ -27,10 +27,12 @@ class Source:
         
         The main properties of a Telescope object are listed here: 
         _ src.phase     : 2D map of the phase scaled to the src wavelength corresponding to tel.OPD
-        _ src.nPhoton   : number of photons per m2 per s. if this property is changed after the initialization, the magnitude is automatically updated to the right value. 
-        _ src.fluxMap   : 2D map of the number of photons per pixel per frame (depends on the loop frequency defined by tel.samplingTime)  
+        _ src.nPhoton   : number of photons per m2 per s. if this property is changed after the initialization,
+                            the magnitude is automatically updated to the right value.
+        _ src.fluxMap   : 2D map of the number of photons per pixel per frame (depends on the loop frequency
+                            defined by tel.samplingTime)
                  
-        ************************** EXEMPLE **************************
+        ************************** EXAMPLE **************************
 
         Create a source object in H band with a magnitude 8 and combine it to the telescope
         src = Source(opticalBand = 'H', magnitude = 8) 
@@ -38,77 +40,77 @@ class Source:
 
         
         """
-# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% INITIALIZATION %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% INITIALIZATION %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
   
-        tmp             = self.photometry(opt_band)              # get the photometry properties
-        self.optBand    = opt_band
-        self.wavelength = tmp[0]                                # wavelength in m
-        self.bandwidth  = tmp[1]                                # optical bandwidth
-        self.zeroPoint  = tmp[2]/368                            # zero point
-        self.magnitude  = magnitude                             # magnitude
-        self.phase      = []                                    # phase of the source 
-        self.phase_no_pupil      = []                                    # phase of the source 
-        self.fluxMap    = []                                    # 2D flux map of the source
-        self.nPhoton    = self.zeroPoint*10**(-0.4*magnitude)   # number of photon per m2 per s
-        self.tag        = 'source'                              # tag of the object
-        
-        
-        print('%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% SOURCE %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%')
-        print('Wavelength \t'+str(round(self.wavelength*1e6,3)) + ' \t [microns]') 
-        print('Optical Band \t'+opt_band)
-        print('Magnitude \t' + str(self.magnitude))
-        print('Flux \t\t'+ str(np.round(self.nPhoton)) + str('\t [photons/m2/s]'))
-        print('%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%')
+        tmp = self.photometry(opt_band)                         # get the photometry properties
+        self.opt_band = opt_band                                # optical band
+        self.wavelength = tmp["wl"]                             # wavelength in m
+        self.bandwidth = tmp["bw"]                              # optical bandwidth
+        self.zero_point = tmp["zp"]                             # zero point
+        self.magnitude = magnitude                              # magnitude
+        self.phase = []                                         # phase of the source
+        self.phase_no_pupil = []                                # phase of the source
+        self.flux_map = []                                      # 2D flux map of the source
+        self.n_photon = self.zero_point*10**(-0.4*magnitude)    # number of photon per m2 per s
+        self.tag = 'source'                                     # tag of the object
 
-# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% SOURCE INTERACTION %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 
+        print(f"%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% SOURCE %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\n"
+              f"Wavelength \t{round(self.wavelength*1e6, 3)} \t [microns]\n"
+              f"Optical Band \t{opt_band}\n"
+              f"Magnitude \t{self.magnitude}\n"
+              f"Flux \t\t{np.round(self.n_photon)}\t [photons/m2/s]\n"
+              '%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%')
+
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% SOURCE INTERACTION %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
         
-    def __mul__(self,telObject):
+    def __mul__(self, tel_object):
         # update the phase of the source
-        self.phase      = telObject.OPD*2*np.pi/self.wavelength
-        self.phase_no_pupil      = telObject.OPD_no_pupil*2*np.pi/self.wavelength
+        self.phase = tel_object.OPD * 2 * np.pi / self.wavelength
+        self.phase_no_pupil = tel_object.OPD_no_pupil * 2 * np.pi / self.wavelength
 
         # compute the variance in the pupil
-        self.var        = np.var(self.phase[np.where(telObject.pupil==1)])
+        self.var = np.var(self.phase[np.where(tel_object.pupil == 1)])
         # assign the source object to the telescope object
-        telObject.src   = self
+        tel_object.src = self
 
-        self.fluxMap    = telObject.pupilReflectivity*self.nPhoton*telObject.samplingTime*(telObject.D/telObject.resolution)**2
+        self.fluxMap = tel_object.pupilReflectivity * self.n_photon*tel_object.samplingTime\
+                       * (tel_object.D/tel_object.resolution)**2
         
-        return telObject
-     
-    
-# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% SOURCE PHOTOMETRY %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 
+        return tel_object
 
-    def photometry(self,arg):
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% SOURCE PHOTOMETRY %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    @staticmethod
+    def photometry(arg):
         # photometry object [wavelength, bandwidth, zeroPoint]
-        phot = Phot()
-        if isinstance(arg,str):
-            if hasattr(phot,arg):
-                return getattr(phot,arg)
+        phot = Photometry()
+        if isinstance(arg, str):
+            if hasattr(phot, arg):
+                return getattr(phot, arg)
             else:
-                print('Error: Wrong name for the photometry object')
+                print("Error: Wrong name for the photometry object.")
                 return -1
         else:
-            print('Error: The photometry object takes a scalar as an input')
-            return -1   
-# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% SOURCE PROPERTIES %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 
+            print("Error: The photometry object takes a scalar as an input.")
+            return -1
+
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% SOURCE PROPERTIES %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
     @property
-    def nPhoton(self):
-        return self._nPhoton
+    def n_photon(self):
+        return self._n_photon
     
-    @nPhoton.setter
-    def nPhoton(self,val):
-        self._nPhoton  = val
-        self.magnitude = -2.5*np.log10(val/self.zeroPoint)
-        print('NGS flux updated!')
-        print('%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% SOURCE %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%')
-        print('Wavelength \t'+str(round(self.wavelength*1e6,3)) + ' \t [microns]') 
-        print('Optical Band \t'+self.optBand) 
-        print('Magnitude \t' + str(self.magnitude))
-        print('Flux \t\t'+ str(np.round(self.nPhoton)) + str('\t [photons/m2/s]'))
-        print('%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%')
-        
+    @n_photon.setter
+    def n_photon(self, val):
+        self._n_photon = val
+        self.magnitude = -2.5*np.log10(val/self.zero_point)
+        print("NGS flux updated!"
+              "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% SOURCE %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
+              f"Wavelength \t{round(self.wavelength*1e6, 3)}\t [microns]"
+              f"Optical Band \t{self.opt_band}"
+              f"Magnitude \t{self.magnitude}"
+              f"Flux \t\t{np.round(self.n_photon)}\t [photons/m2/s]')"
+              "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
+
 #    @property
 #    def magnitude(self):
 #        return self._magnitude
@@ -118,21 +120,20 @@ class Source:
 #        self._magnitude  = val
 #        self.nPhoton     = self.zeroPoint*10**(-0.4*val)
 #            
-# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% END %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% END %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
  
     def show(self):
-        attributes = inspect.getmembers(self, lambda a:not(inspect.isroutine(a)))
+        attributes = inspect.getmembers(self, lambda a: not(inspect.isroutine(a)))
         print(self.tag+':')
         for a in attributes:
             if not(a[0].startswith('__') and a[0].endswith('__')):
                 if not(a[0].startswith('_')):
                     if not np.shape(a[1]):
-                        tmp=a[1]
+                        tmp = a[1]
                         try:
                             print('          '+str(a[0])+': '+str(tmp.tag)+' object') 
                         except:
                             print('          '+str(a[0])+': '+str(a[1])) 
                     else:
-                        if np.ndim(a[1])>1:
+                        if np.ndim(a[1]) > 1:
                             print('          '+str(a[0])+': '+str(np.shape(a[1])))   
-            
