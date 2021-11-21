d3722574938520294101d510e8bc06de9c6e596a
J. Aveiro <jpd.aveiro@gmail.com>
2021-11-21 15:41:35 +0000

    Split photo/Source classes
    - Change name from photo to Photo (classname should be capital case)
    - Create separate file with photometry (Photo) class
    - Change argument name optBand to opt_band (argument name should not be camelcase)

--- AO_modules/source.py
+++ AO_modules/source.py
@@ -1,15 +1,14 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Feb 19 10:32:15 2020
-
-@author: cheritie
-"""
-import numpy as np
 import inspect
-# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% CLASS INITIALIZATION %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 
+
+import numpy as np
+
+from photometry import Phot
+
+
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% CLASS INITIALIZATION %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 class Source:    
-    def __init__(self,optBand,magnitude):
+    def __init__(self, opt_band, magnitude):
         """
         ************************** REQUIRED PARAMETERS **************************
         
@@ -41,8 +40,8 @@ class Source:
         """
 # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% INITIALIZATION %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 
   
-        tmp             = self.photometry(optBand)              # get the photometry properties
-        self.optBand    = optBand
+        tmp             = self.photometry(opt_band)              # get the photometry properties
+        self.optBand    = opt_band
         self.wavelength = tmp[0]                                # wavelength in m
         self.bandwidth  = tmp[1]                                # optical bandwidth
         self.zeroPoint  = tmp[2]/368                            # zero point
@@ -56,7 +55,7 @@ class Source:
         
         print('%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% SOURCE %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%')
         print('Wavelength \t'+str(round(self.wavelength*1e6,3)) + ' \t [microns]') 
-        print('Optical Band \t'+optBand) 
+        print('Optical Band \t'+opt_band)
         print('Magnitude \t' + str(self.magnitude))
         print('Flux \t\t'+ str(np.round(self.nPhoton)) + str('\t [photons/m2/s]'))
         print('%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%')
@@ -82,37 +81,7 @@ class Source:
 
     def photometry(self,arg):
         # photometry object [wavelength, bandwidth, zeroPoint]
-        class phot:
-            pass
-        
-        phot.U      = [ 0.360e-6 , 0.070e-6 , 2.0e12 ]
-        phot.B      = [ 0.440e-6 , 0.100e-6 , 5.4e12 ]
-        phot.V0     = [ 0.500e-6 , 0.090e-6 , 3.3e12 ]
-        phot.V      = [ 0.550e-6 , 0.090e-6 , 3.3e12 ]
-        phot.R      = [ 0.640e-6 , 0.150e-6 , 4.0e12 ]
-        phot.I      = [ 0.790e-6 , 0.150e-6 , 2.7e12 ]
-        phot.I1     = [ 0.700e-6 , 0.033e-6 , 2.7e12 ]
-        phot.I2     = [ 0.750e-6 , 0.033e-6 , 2.7e12 ]
-        phot.I3     = [ 0.800e-6 , 0.033e-6 , 2.7e12 ]
-        phot.I4     = [ 0.700e-6 , 0.100e-6 , 2.7e12 ]
-        phot.I5     = [ 0.850e-6 , 0.100e-6 , 2.7e12 ]
-        phot.I6     = [ 1.000e-6 , 0.100e-6 , 2.7e12 ]
-        phot.I7     = [ 0.850e-6 , 0.300e-6 , 2.7e12 ]
-        phot.R2     = [ 0.650e-6 , 0.300e-6 , 7.92e12 ]
-        phot.R3     = [ 0.600e-6 , 0.300e-6 , 7.92e12 ]
-        phot.R4     = [ 0.670e-6 , 0.300e-6 , 7.92e12 ]
-        phot.I8     = [ 0.750e-6 , 0.100e-6 , 2.7e12 ]
-        phot.I9     = [ 0.850e-6 , 0.300e-6 , 7.36e12 ]
-        phot.J      = [ 1.215e-6 , 0.260e-6 , 1.9e12 ]
-        phot.H      = [ 1.654e-6 , 0.290e-6 , 1.1e12 ]
-        phot.Kp     = [ 2.1245e-6 , 0.351e-6 , 6e11 ]
-        phot.Ks     = [ 2.157e-6 , 0.320e-6 , 5.5e11 ]
-        phot.K      = [ 2.179e-6 , 0.410e-6 , 7.0e11 ]
-        phot.L      = [ 3.547e-6 , 0.570e-6 , 2.5e11 ]
-        phot.M      = [ 4.769e-6 , 0.450e-6 , 8.4e10 ]
-        phot.Na     = [ 0.589e-6 , 0        , 3.3e12 ]
-        phot.EOS    = [ 1.064e-6 , 0        , 3.3e12 ]
-        
+        phot = Phot()
         if isinstance(arg,str):
             if hasattr(phot,arg):
                 return getattr(phot,arg)
